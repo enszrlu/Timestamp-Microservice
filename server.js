8e59b78e249ffe738d4f1717e0144a6d6dceddde
enszrlu <enes.zrl@gmail.com>
2022-03-13 17:07:23 +0300

    completed solution

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,12 +21,30 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
+});
+
+// UNIX
+app.get("/api/:time", function (req, res) {
+  let date = new Date(req.params.time);
+  if (date.getTime() > 0) {
+    res.json({
+      "unix": date.valueOf(),
+      "utc": date.toString()
+    });
+  }
+  else {
+    let date = new Date(req.params.time * 1000);
+    res.json({
+      "unix": date.valueOf(),
+      "utc": date.toString()
+    });
+  }
+
 });
 
 
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
